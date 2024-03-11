9fea7970b5d5ca5e17a66dd9a88699b7c87dd1e4
Jeff Marks <jeff.t.marks@gmail.com>
2024-03-11 09:10:18 -0400

    Deactivate void command

--- deactivated_commands/void.js
+++ deactivated_commands/void.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
-const Theme = require('../../models/theme');
-const { modifyResponse } = require('../../utils/themeHelper');
+const Theme = require('../models/theme');
+const { modifyResponse } = require('../utils/themeHelper');
 
 module.exports = {
 	data: new SlashCommandBuilder()
